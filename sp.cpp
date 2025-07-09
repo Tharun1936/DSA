4b5fc38d60e76b1211d99c1b1f526c79af1c11d3
Tharun A M <tharunmgowda1936@gmail.com>
2025-07-09 12:09:15 +0530

    Add files via upload

---
+++ sp.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char ch;
+//     cout<<"Enter the word"<<endl;
+//     cin>>ch;
+//     if(ch>='a' && ch<='z'){
+//         cout<<"Lower case"<<endl;
+//     }
+//     else if (ch>= 'A' && ch<= 'Z'){
+//         cout<<"Upper Case"<<endl;
+//     }
+//     else{
+//         cout<<"error"<<endl;
+//     }
+//     return 0;
+// }
+
+// ternary operator:-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 19;
+    cout<<(a>=18 ? "You can drive": "YOu cant")<<endl;
+}
