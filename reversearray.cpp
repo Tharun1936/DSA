3d5dc12896b2005ca3cda3af559f7f7a268ccc44
Tharun A M <tharunmgowda1936@gmail.com>
2025-09-08 22:51:21 +0530

    Add files via upload

---
+++ reversearray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4};
+    int size=4;
+    int start=0;
+    int end=size-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+    for(int i=0;i<size;i++){
+        cout<<arr[i];
+    }
+    cout<<endl;
+
+}
