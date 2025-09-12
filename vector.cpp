a4df430072c9ca2f2e295de84f35ee8a7e6af070
Tharun A M <tharunmgowda1936@gmail.com>
2025-09-12 23:18:28 +0530

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,44 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int> vec(4,0);
+//     for(int i:vec){
+//         cout<<i;
+//     }
+//     cout<<endl;
+    
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<char> vec={'a','b','c'};
+//     for(char i:vec){
+//         cout<<i;
+//     }
+//     cout<<endl;
+    
+// }
+
+
+//vector functions
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<char> vec;
+//     cout<<vec.size()<<endl;
+//     vec.push_back(20);
+//     vec.push_back(21);
+//     vec.push_back(22);
+//     vec.pop_back();// last index will pop
+//     cout<<vec.size()<<endl;
+//     cout<<vec.front()<<endl;
+//     cout<<vec.back()<<endl;
+//     cout<<vec.at(0)<<endl;
+    
+// }
+
+
