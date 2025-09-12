4e7d59e336628f3769f6d3bd25abcadf8ab73c36
Tharun A M <tharunmgowda1936@gmail.com>
2025-09-12 23:18:52 +0530

    Add files via upload

---
+++ singlenum.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int> num={1,4,1,3,3};
+//     int ans=0;
+//     for(int val:num){
+//         ans=ans^val;
+//     }
+//     cout<<ans<<endl;
+// }
+
+// linear search using vectors
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> num={1,4,1,3,3};
+    int target=4;
+    for(int val:nums){
+        if(val==target){
+            return val;
+        }
+    }
+        
+}
